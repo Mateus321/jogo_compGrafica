d7f768165da8e754e484dae2a717b53550a37533
Daniel <mescolind@gmail.com>
2023-10-02 10:16:02 -0300

    novo carro em obb

---
+++ cybertruckobj.js
@@ -0,0 +1,281 @@
+import * as THREE from  'three';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import { Pista } from './pistaarray.js';
+
+import {initRenderer, 
+        initCamera,
+        initDefaultBasicLight,
+        setDefaultMaterial,
+        InfoBox,
+        SecondaryBox,        
+        onWindowResize, 
+        createGroundPlaneXZ} from "../libs/util/util.js";
+import { ConvexGeometry } from '../build/jsm/geometries/ConvexGeometry.js';
+
+// Listen window size changes
+
+// Use to scale the cube
+var scale = 1.0;
+
+// Show text information onscreen
+
+
+// To use the keyboard
+
+
+// Show axes (parameter is size of each axis)
+
+// create the ground plane
+// scene.add(plane);
+
+
+
+
+export class Carro {
+    constructor(scene, inicial, keyboard) {
+        this.scene = scene
+        this.inicial = inicial;
+        this.keyboard = keyboard;
+        this.checkpointsVisitados = [];
+        this.voltas = 0;
+        this.temp = 1;
+        this.cron;
+        this.ms = 0;
+        this.mm = 0;
+        this.ss = 0;
+        this.tempo = [''];
+
+        this.tempVolta = 1;
+        this.cronVolta;
+        this.msV = 0;
+        this.mmV = 0;
+        this.ssV = 0;
+        this.tempoV = [''];
+
+
+
+        this.carro = new THREE.Object3D();
+
+        this.criaBase = (pontos) => {
+            const baseGeometry = new ConvexGeometry(pontos);
+            const baseMaterial = new THREE.MeshPhongMaterial( {color: 0xF0130A });
+            const base = new THREE.Mesh(baseGeometry, baseMaterial);
+            return base;
+        }
+
+        this.carro.position.set(inicial[0], inicial[1]+0.475, inicial[2]);
+        this.carro.scale.set(0.15,0.15,0.15);
+
+        this.scene.add(this.carro);
+
+        this.aceleracao = -0.0008;
+        
+        this.velocidade = 0;
+
+        this.limiteVelocidade = -0.16;
+
+        this.acelerar = () =>{
+            if(this.velocidade > this.limiteVelocidade){
+                this.velocidade += this.aceleracao;
+                this.velocidade = Number(this.velocidade.toFixed(4));
+            }
+            this.carro.translateX(this.velocidade);
+        }
+
+        this.desacelerar = () =>{
+            if(this.velocidade < 0){
+                this.velocidade -= this.aceleracao;
+                this.velocidade = Number(this.velocidade.toFixed(4));
+                }
+            this.carro.translateX(this.velocidade);
+        }
+
+        this.freiar = () => {
+         this.velocidade -= this.aceleracao*3;
+         this.velocidade = Number(this.velocidade.toFixed(4));
+        }
+        
+
+        this.re = () => {
+            if(this.velocidade < 0) this.freiar();
+            else{
+                if(this.velocidade <= -this.limiteVelocidade/2.4){
+                    this.velocidade -= this.aceleracao;
+                    this.velocidade = Number(this.velocidade.toFixed(4));
+                }
+                this.carro.translateX(this.velocidade);
+            }
+        }
+
+        this.desacelerarRe = () => {
+            if(this.velocidade > 0){
+                this.velocidade += this.aceleracao;
+                this.velocidade = Number(this.velocidade.toFixed(4));
+                }
+            this.carro.translateX(this.velocidade);
+        }
+
+        this.estaNaPista = (pista) => {
+            let posicoesVector3 = []
+            for(let i = 0; i < pista.pista.children.length; i++){
+                posicoesVector3[i] = pista.pista.children[i].position;
+                let carroNaPista = posicoesVector3.some((posicaoVector3) => {
+                    const distanciaLimite = 7;
+                    let distancia = this.carro.position.distanceTo(posicaoVector3);
+                    return distancia < distanciaLimite
+                });
+                if(carroNaPista){
+                    return true;
+                    
+                } 
+            }
+            return false;  
+        }
+
+        this.penalidade = (pista) => {
+            if(!this.estaNaPista(pista)){
+            this.limiteVelocidade = -0.08;
+            if(this.velocidade < this.limiteVelocidade){
+                this.velocidade = this.limiteVelocidade;
+            }
+            }else{
+                this.limiteVelocidade = -0.16;
+            }
+        }
+
+        this.pause = () => {
+            clearInterval(this.cron);
+            this.aceleracao = 0;
+        }
+
+        this.stop = () => {
+            clearInterval(this.cron);
+            this.ss = 0;
+            this.ms = 0;
+            this.mm = 0;
+        }
+
+        this.resetPos = () => {
+            this.carro.position.set(inicial[0], inicial[1]+0.475, inicial[2]);
+            this.carro.rotation.x = 0;
+            this.carro.rotation.y = 0;
+            this.carro.rotation.z = 0;
+        }
+
+        this.reset = () => {
+            this.resetPos();
+            this.velocidade = 0;
+            this.voltas = 0;
+            this.tempo[0] = "00:00"
+            clearInterval(this.cron);
+            this.ms = 0;
+            this.ss = 0;
+            this.mm = 0;
+            this.start();
+        }   
+        
+        this.start = () => {
+            this.cron = setInterval(() => {this.time()}, this.temp);
+        } 
+
+        this.time = ( ) => {
+            this.ms++;
+          
+            if(this.ms == 60)
+            {
+              this.ms = 0;
+              this.ss++;
+              if(this.ss == 60){
+                this.ss = 0;
+                this.mm++;
+                
+              }
+            }
+            
+            let format = (this.mm < 10 ? '0' + this.mm : this.mm) + ':' + (this.ss < 10 ? '0' + this.ss : this.ss) + ':' + (this.ms < 10 ? '0' + this.ms : this.ms);
+            
+            this.tempo[0] = format;
+          }
+
+          this.resetVolta = () => {
+            clearInterval(this.cronVolta);
+            this.msV = 0;
+            this.ssV = 0;
+            this.mmV = 0;
+            this.startVolta(); 
+        }   
+        
+        this.startVolta = () => {
+            this.cronVolta = setInterval(() => {this.timeVolta()}, this.tempVolta);
+        } 
+
+        this.timeVolta = ( ) => {
+            this.msV++;
+          
+            if(this.msV == 60)
+            {
+              this.msV = 0;
+              this.ssV++;
+              if(this.ssV == 60){
+                this.ssV = 0;
+                this.mmV++;
+                
+              }
+            }
+            
+            let formatV = (this.mmV < 10 ? '0' + this.mmV : this.mmV) + ':' + (this.ssV < 10 ? '0' + this.ssV : this.ssV) + ':' + (this.msV < 10 ? '0' + this.msV : this.msV);
+            
+            this.tempoV[0] = formatV;
+          }
+
+        
+
+        this.keyboardUpdate = () => {
+
+            let angle = THREE.MathUtils.degToRad(5);
+            let maxRotation = Math.PI / 6;
+
+            this.keyboard.update();
+          
+            if ( this.keyboard.pressed("X") ) this.acelerar();
+            else this.desacelerar();
+            if ( this.keyboard.pressed("down") )  this.re();
+            else this.desacelerarRe();
+          
+            if ( this.keyboard.pressed("left") ){ 
+                if(this.velocidade < 0) this.carro.rotateY(  angle/4 );
+                else if(this.velocidade > 0) this.carro.rotateY( -angle/4 );
+                if(calota_frente_direita.rotation.z >= -maxRotation){
+                calota_frente_direita.rotateZ(-angle/2);
+                calota_frente_esquerda.rotateZ(-angle/2);
+                }
+            }  
+            else if ( this.keyboard.pressed("right") ){
+                if(this.velocidade < 0) this.carro.rotateY( -angle/4 );
+                else if(this.velocidade > 0) this.carro.rotateY( angle/4 );
+                if(calota_frente_direita.rotation.z <= maxRotation){
+                calota_frente_direita.rotateZ(angle/2);
+                calota_frente_esquerda.rotateZ(angle/2);
+                }
+            }else{
+                if(calota_frente_direita.rotation.z != 0){
+                    if(calota_frente_direita.rotation.z > 0){
+                        calota_frente_direita.rotateZ(-angle/2);
+                        calota_frente_esquerda.rotateZ(-angle/2);
+                    }else{
+                        calota_frente_direita.rotateZ(angle/2);
+                        calota_frente_esquerda.rotateZ(angle/2);
+                    }
+                }
+            }
+          }   
+    }
+
+
+
+}
+        
+
+
+
+
---
+++ main.js
@@ -0,0 +1,240 @@
+import * as THREE from  'three';
+import { Carro } from './carro.js';
+import {Pista, listaPistas} from './pistaarray.js';
+import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import KeyboardState from '../libs/util/KeyboardState.js';
+import {initRenderer, 
+        initCamera,
+        initDefaultSpotlight,
+        initDefaultBasicLight,
+        SecondaryBox,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneXZ} from "../libs/util/util.js";
+
+let inspec = false;
+
+let scene, renderer, camera, material, light, orbit; // Initial variables
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
+//camera = initCamera(new THREE.Vector3(30,15,45)); // Init camera in this position
+camera = initCamera(new THREE.Vector3(27,6,50)); // Init camera in this position
+camera.lookAt(40,0,20);
+light = initDefaultBasicLight(scene); // Enable mouse rotation, pan, zoom etc.
+// initDefaultSpotlight(scene, new THREE.Vector3(35, 20, 30)); // Use default light
+
+let inspecCamera = initCamera(new THREE.Vector3(-45,20,140));
+//inspecCamera.lookAt(150,100,200);
+orbit = new OrbitControls( inspecCamera, renderer.domElement );
+
+let pistaEscolhida = 0;
+
+
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// Show axes (parameter is size of each axis)
+let plane = createGroundPlaneXZ(200, 200);
+scene.add(plane);
+
+
+var keyboard = new KeyboardState();
+
+let trackballControls = new TrackballControls( camera, renderer.domElement );
+let inspecTrackballControls = new TrackballControls( inspecCamera, renderer.domElement );
+
+
+let voltasMessage = new SecondaryBox("");
+let tempMessage = new SecondaryBox("");
+let volta1Message = new SecondaryBox("");
+let volta2Message = new SecondaryBox("");
+let volta3Message = new SecondaryBox("");
+let volta4Message = new SecondaryBox("");
+
+
+let pista = new Pista(listaPistas[pistaEscolhida].id, listaPistas[pistaEscolhida].posicoes, listaPistas[pistaEscolhida].checkpoints, scene);
+const carro = new Carro(scene, pista.getInicial(), keyboard);
+
+const distanciaCamera = carro.carro.position.distanceToSquared(camera.position);
+
+const distanciaCameraX = camera.position.x - carro.carro.position.x;
+const distanciaCameraZ = (camera.position.z - carro.carro.position.z);
+
+voltasMessage.changeStyle("rgba(0,0,0,0)", "white", "32px", "ubuntu")
+voltasMessage.box.style.bottom = "92%";
+voltasMessage.box.style.left = "2%";
+
+tempMessage.changeStyle("rgba(0,0,0,0)", "white", "32px", "ubuntu")
+tempMessage.box.style.bottom = "88%";
+tempMessage.box.style.left = "2%";
+
+volta1Message.changeStyle("rgba(0,0,0,0)", "white", "32px", "ubuntu")
+volta1Message.box.style.bottom = "84%";
+volta1Message.box.style.left = "2%";
+
+volta2Message.changeStyle("rgba(0,0,0,0)", "white", "32px", "ubuntu")
+volta2Message.box.style.bottom = "80%";
+volta2Message.box.style.left = "2%";
+
+volta3Message.changeStyle("rgba(0,0,0,0)", "white", "32px", "ubuntu")
+volta3Message.box.style.bottom = "76%";
+volta3Message.box.style.left = "2%";
+
+volta4Message.changeStyle("rgba(0,0,0,0)", "white", "32px", "ubuntu")
+volta4Message.box.style.bottom = "72%";
+volta4Message.box.style.left = "2%";
+
+
+function updateVoltasMessage()
+{
+   let str =  "Voltas: " + carro.voltas;
+   if(carro.voltas == 4){
+    carro.pause();
+    str = "Voce finalizou a corrida!!!"
+    carro.velocidade = 0.0000;
+   }
+   voltasMessage.changeMessage(str);
+}
+
+function updateTempMenssage()
+{
+  let str, str1, str2, str3, str4;
+    switch(carro.tempo.length){
+      case 1:
+        str = "Tempo total: " + carro.tempo[0];
+        break;
+      case 2:
+        str = "Tempo total: " + carro.tempo[0];
+        str1 = "Volta 1: " + carro.tempoV[1];
+        
+        break;
+      case 3:
+        str = "Tempo total: " + carro.tempo[0];
+        str1 = "Volta 1: " + carro.tempoV[1];
+        str2 = "Volta 2: " + carro.tempoV[2];
+        break;
+      case 4:
+        str = "Tempo total: " + carro.tempo[0];
+        str1 = "Volta 1: " + carro.tempoV[1];
+        str2 = "Volta 2: " + carro.tempoV[2];
+        str3 = "Votla 3: " + carro.tempoV[3];
+        break;
+      case 5:
+        str = "Tempo total: " + carro.tempo[0];
+        str1 = "Volta 1: " + carro.tempoV[1];
+        str2 = "Volta 2: " + carro.tempoV[2];
+        str3 = "Volta 3: " + carro.tempoV[3];
+        str4 = "Volta 4: " + carro.tempoV[4];
+        break;
+        
+
+    }
+    tempMessage.changeMessage(str);
+    volta1Message.changeMessage(str1);
+    volta2Message.changeMessage(str2);
+    volta3Message.changeMessage(str3);
+    volta4Message.changeMessage(str4);
+}
+
+
+const trocaPista = () => {
+  if(pista){
+    pista.removePista();
+  }
+  const novaPista = new Pista(listaPistas[pistaEscolhida].id, listaPistas[pistaEscolhida].posicoes, listaPistas[pistaEscolhida].checkpoints, scene);
+  let inicial = novaPista.getInicial();
+  carro.reset();
+  return novaPista;
+}
+
+const hideMessages =  () => {
+  tempMessage.hide();
+  volta1Message.hide();
+  volta2Message.hide();
+  volta3Message.hide();
+  volta4Message.hide();
+}
+
+const toggleInspec = () => {
+  if(!inspec){
+  carro.carro.scale.set(10,10,10);
+  scene.remove(plane);
+  carro.reset();
+  pista.removePista();
+  hideMessages();
+  }else{
+    carro.carro.scale.set(0.15,0.15,0.15);
+    scene.add(plane);
+    pista = new Pista(listaPistas[pistaEscolhida].id, listaPistas[pistaEscolhida].posicoes, listaPistas[pistaEscolhida].checkpoints, scene);
+  }
+  inspec = !inspec;
+}
+
+const keyboardUpdate = () => {
+  keyboard.update();
+  if(keyboard.down("space")) toggleInspec();
+  if(keyboard.down("1")){
+    pistaEscolhida = 0;
+    pista = trocaPista();
+    carro.tempo.length = 0;
+    carro.aceleracao = -0.0008;
+  }
+  if(keyboard.down("2")){
+    pistaEscolhida = 1;
+    pista = trocaPista();
+    carro.tempo.length = 0;
+    carro.aceleracao = -0.0008;
+  } 
+}
+
+
+
+
+
+
+function updateCameraLookAt(){
+  camera.lookAt(carro.carro.position);
+}
+// posição da camera = distancia fixa + posicão do carro
+function updateCameraPosition(){
+  if(distanciaCamera != carro.carro.position.distanceToSquared(camera.position)){
+    updateCameraLookAt();
+    camera.position.set(carro.carro.position.x + distanciaCameraX, camera.position.y, carro.carro.position.z + distanciaCameraZ);
+  }
+}
+carro.start();
+carro.startVolta();
+render();
+
+function render()
+{
+  
+  keyboardUpdate();
+  requestAnimationFrame(render);
+  if(!inspec){
+    trackballControls.update();
+    trackballControls.target.copy(carro.carro.position);
+    carro.keyboardUpdate();
+    updateVoltasMessage();
+    updateTempMenssage();
+    updateCameraPosition();
+    carro.penalidade(pista);
+    if(pista.checkpointsVisitados(carro)){
+      
+      carro.tempo.push(carro.tempo[0]);
+      carro.tempoV.push(carro.tempoV[0]);
+      carro.voltas += 1;
+      carro.checkpointsVisitados = [];
+      pista.proximoCheckpoint = 0;
+      carro.resetVolta();
+    }
+    renderer.render(scene, camera) 
+  }else{
+    renderer.render(scene, inspecCamera);
+    console.log(inspecCamera);
+    // inspecTrackballControls.update();
+  }
+}// Render scene
+  
